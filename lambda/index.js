edc90f94147db6eed009adf7bad30db8db8ff8ac
Marcin Sikon <marcin.sikon@gmail.com>
2017-10-17 15:27:30 +0200

    Obido.pl - Custom flow to resizing

--- lambda/index.js
+++ lambda/index.js
@@ -2,38 +2,57 @@
 
 const AWS = require('aws-sdk');
 const S3 = new AWS.S3({
-  signatureVersion: 'v4',
+    signatureVersion: 'v4',
 });
 const Sharp = require('sharp');
 
 const BUCKET = process.env.BUCKET;
 const URL = process.env.URL;
 
-exports.handler = function(event, context, callback) {
-  const key = event.queryStringParameters.key;
-  const match = key.match(/(\d+)x(\d+)\/(.*)/);
-  const width = parseInt(match[1], 10);
-  const height = parseInt(match[2], 10);
-  const originalKey = match[3];
+exports.handler = function (event, context, callback) {
+    const key = event.queryStringParameters.key;
+    const match = key.match(/(\d+)x(\d+)x(\d+)\/(.*)/);
+    let width = parseInt(match[1], 10);
+    let height = parseInt(match[2], 10);
+    const quality = parseInt(match[3], 10);
+    const originalKey = match[4];
 
-  S3.getObject({Bucket: BUCKET, Key: originalKey}).promise()
-    .then(data => Sharp(data.Body)
-      .resize(width, height)
-      .toFormat('png')
-      .toBuffer()
-    )
-    .then(buffer => S3.putObject({
-        Body: buffer,
-        Bucket: BUCKET,
-        ContentType: 'image/png',
-        Key: key,
-      }).promise()
-    )
-    .then(() => callback(null, {
-        statusCode: '301',
-        headers: {'location': `${URL}/${key}`},
-        body: '',
-      })
-    )
-    .catch(err => callback(err))
-}
+    if (height === 0) {
+        height = 4096;
+    }
+
+    if (width === 0) {
+        width = 4096;
+    }
+
+    console.log(key, originalKey, width, height, quality, match);
+
+    S3.getObject({Bucket: BUCKET, Key: originalKey}).promise()
+        .then(data => Sharp(data.Body)
+            .resize(width, height)
+            .max()
+            .toFormat('jpeg', {'quality': quality})
+            .toBuffer()
+        )
+        .then(buffer => S3.putObject({
+                Body: buffer,
+                Bucket: BUCKET,
+                CacheControl: "max-age=94608000",
+                ContentType: 'image/jpeg',
+                Tagging: "resized=true",
+                Key: key,
+            }).promise()
+        )
+        .then(() => callback(null, {
+                statusCode: '301',
+                headers: {
+                    'location': `${URL}/${key}`,
+                    'Access-Control-Allow-Origin': '*',
+                    'access-control-allow-methods': 'GET',
+                    'access-control-max-age': '3000'
+                },
+                body: '',
+            })
+        )
+        .catch(err => callback(err))
+};
